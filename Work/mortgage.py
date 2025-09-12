68ffd51344281feeec6846bcf95aca7046d203d4
strgzn <86257693+strgzn@users.noreply.github.com>
2025-09-13 02:33:49 +0300

    Update mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -3,10 +3,25 @@ mortgage = 500000
 rate = 0.05
 monthly = 2684.11
 total = 0
+months = 0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
 
 while mortgage > 0:
-    mortgage = mortgage * (1 + rate / 12) - monthly
+    months += 1
+    if months >= extra_payment_start_month and months <= extra_payment_end_month:
+        mortgage = mortgage * (1 + rate / 12) - (monthly + extra_payment)
+    else:
+        mortgage = mortgage * (1 + rate / 12) - monthly
     total += monthly
+
+    if mortgage < 0:
+        total += mortgage
+        mortgage = 0
+
+    print(months, round(total, 2), round(mortgage, 2))
     
-print("total paid", round(total, 1))
+print("total paid", round(total, 2))
+print("number of months", months)
 # Exercise 1.7
