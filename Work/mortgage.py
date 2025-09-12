3d03692de961e61026cf2af3770b727bd4d59327
strgzn <86257693+strgzn@users.noreply.github.com>
2025-09-13 02:06:34 +0300

    Update mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,12 @@
 # mortgage.py
-#
+mortgage = 500000
+rate = 0.05
+monthly = 2684.11
+total = 0
+
+while mortgage > 0:
+    mortgage = mortgage * (1 + rate / 12) - monthly
+    total += monthly
+    
+print("total paid", round(total, 1))
 # Exercise 1.7
